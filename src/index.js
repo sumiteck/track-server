d019c5463d913949f74417c076fcfea4e0bd604d
sumitk68 <imsumitk1@gmail.com>
2021-07-14 22:40:56 -0400

    mongo db implemented and connected

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+const mongoUri = "mongodb+srv://sumit-prod:passwordpassword@cluster0.0dw7l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+});
+
+mongoose.connection.on("connected", () => {
+    console.log("Connected to mongo");
+});
+
+mongoose.connection.on("error", (err) => {
+    console.log("Connected to mongo", err);
+});
+
+
 
 app.get("/", (req, res) => {
 
