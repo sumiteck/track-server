78add3de38130574365b78fd9e2effc09d7a37c4
sumitk68 <imsumitk1@gmail.com>
2021-07-31 21:53:33 -0400

    track list backend implemented

--- src/index.js
+++ src/index.js
@@ -1,35 +1,35 @@
-require("./models/User");
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const authRoutes = require("./routes/authRoutes");
-const requireAuth = require("./middlewares/requireAuth");
- 
+require('./models/User');
+require('./models/Track');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+const trackRoutes = require('./routes/trackRoutes');
+const requireAuth = require('./middlewares/requireAuth');
+
 const app = express();
- 
+
 app.use(bodyParser.json());
 app.use(authRoutes);
- 
-const mongoUri = "mongodb+srv://sumit-prod:passwordpassword@cluster0.0dw7l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
- 
+app.use(trackRoutes);
+
+const mongoUri = "mongodb+srv://sumit-prod:passwordpassword@cluster0.0dw7l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 mongoose.connect(mongoUri, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
 });
- 
-mongoose.connection.on("connected", () => {
-  console.log("Connected to mongo instance");
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo instance');
 });
- 
-mongoose.connection.on("error", (error) => {
-  console.error("Error connecting to mongo", error);
+mongoose.connection.on('error',err => {
+    console.error('Error connecting to mongo', err);
 });
- 
-app.get("/", requireAuth, (req, res) => {
-  res.send(`Your email: ${req.user.email}`);
+
+app.get('/' , requireAuth ,(req,res) => {
+    res.send(`Your email: ${req.user.email}`);
 });
- 
+
 app.listen(3000, () => {
-  console.log("Listening on port 3000");
+    console.log('Listening on port 3000');
 });
--- src/middlewares/requireAuth.js
+++ src/middlewares/requireAuth.js
@@ -1,50 +1,25 @@
-const jwt = require("jsonwebtoken");
-const mongoose = require("mongoose");
-const router = require("../routes/authRoutes");
-const User = mongoose.model("User");
- 
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
- 
+  // authorization === 'Bearer laksjdflaksdjasdfklj'
+
   if (!authorization) {
-    return res.status(401).send({ error: "You must be logged in." });
+    return res.status(401).send({ error: 'You must be logged in.' });
   }
- 
-  const token = authorization.replace("Bearer ", "");
-  jwt.verify(token, "MY_SECRET_KEY", async (err, payload) => {
+
+  const token = authorization.replace('Bearer ', '');
+  jwt.verify(token, 'MY_SECRET_KEY', async (err, payload) => {
     if (err) {
-      return res.status(401).send({ error: "You must be logged in." });
+      return res.status(401).send({ error: 'You must be logged in.' });
     }
- 
+
     const { userId } = payload;
 
     const user = await User.findById(userId);
-    console.log(user)
- 
     req.user = user;
     next();
-  });
-
-  router.post("/signin", async (req, res) => {
-    const {email, password    } = req.body;
-    if(!email || !password) {
-        return res.status(422).send({error: "must provide email and password"});
-    }
-    const user = await User.findOne({email});
-    if(!user) {
-        return res.status(422).send({error: "Email is not found "});
-    }
-
-    
-
-    try{
-       
-        await user.comparePassword(password);
-        const token = jwt.sign({userId: user._id}, 'MY_SECRET_KEY');
-    
-        res.send({token});
-       }catch(err) {
-           return res.status(422).send({error: "Invalid email or Password"});
-       }
   });
 };
